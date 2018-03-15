f65cc1e39fdb5ee911a80aff97797f275b315e37
浅爱 <shallowlove21@gmail.com>
2018-03-15 16:03:25 +0800

    Initial Commit

---
+++ tableViewDemo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableViewDemo2
+//
+//  Created by PatrickY on 2018/3/15.
+//  Copyright © 2018年 PatrickY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
